61841958fadb71ec35aa4d976abc072d361ce79b
David Hernández <david.hernandezc@iteso.mx>
2023-06-29 22:26:15 -0600

    resources structure

--- ITER1/src/main/java/net/chesterdavid/tutorialmod/item/ModItems.java
+++ ITER1/src/main/java/net/chesterdavid/tutorialmod/item/ModItems.java
@@ -1,17 +1,27 @@
 package net.chesterdavid.tutorialmod.item;
 
 import net.chesterdavid.tutorialmod.TutorialMod;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.IModStateTransition;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems
 {
+    // With this we are kinda creating a object as a Item that the game can see
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
 
     // Adding the item
-
+    /*
+    In this part we are adding the item in the game, is kinda hard to explain the process.
+     -- IN THIS PART WE ARE NOT ADDING THE NAME NEITHER THE TEXTURE -- For those we need to
+     add it in the resources
+     */
+    public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
 
     public static void register(IEventBus eventBus)
     {
